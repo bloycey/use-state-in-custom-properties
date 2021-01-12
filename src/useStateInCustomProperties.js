b5c5cf22f712d759a0d6ff43194c490c0047952a
Andrew Bloyce <bloyce1990@gmail.com>
2021-01-12 13:55:48 +1000

    Initial Commit

---
+++ src/useStateInCustomProperties.js
@@ -0,0 +1,19 @@
+const useStateInCustomProperties = (className, propertiesObject) => {
+	return ({ children }) => {
+		const customPropertyStyles = Object.entries(propertiesObject).reduce(
+			(acc, [key, value]) => ({
+				...acc,
+				[`--${key}`]: value,
+			}),
+			{}
+		);
+
+		return (
+			<div className={className} style={customPropertyStyles}>
+				{children}
+			</div>
+		);
+	};
+};
+
+export default useStateInCustomProperties;
